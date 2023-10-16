37e30206ef5ba2500ab6dddb6fe2102653ac2f89
Laboratorio <laboratorio@udla-ec.int>
2023-10-16 16:49:49 -0500

    gauss

---
+++ gauss.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main ()
+
+{
+int num, sumg;
+
+   
+printf("Ingrese un numero\n");
+    scanf("%d", &num);
+
+    sumg = (num * (num+1))/2;
+
+    printf("La suma de Gauss del numero ingresado es:%d", sumg);
+       
+   return 0;
+}
